b9945cc5dd2db6a61d4a2d49dce48896ba777597
szilard-molnar <mszilard86@gmail.com>
2021-02-13 21:21:17 -0500

    Vue Bootstrap hooked up

--- vue-covid/src/App.vue
+++ vue-covid/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <h1>Covid tracker Vue.js</h1>
+    <div class="title">
+          <h1>Covid tracker Vue.js</h1>
+    </div>
   </div>
 </template>
 
@@ -18,5 +20,14 @@ export default {
   #app {
     height: 100vh;
     background-color: rgb(255, 255, 180);
+  }
+
+  .title {
+    height: 100px;
+    border-bottom: 2px solid rgba(0, 60, 255, 0.612);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(105, 230, 252, 0.194);
   }
 </style>
